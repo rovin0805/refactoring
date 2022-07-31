20e113c5b3171af25b2b37a3c4b4ad98b7500058
Choi Eun Jeong <qhdrn67@naver.com>
2022-07-31 18:52:39 +0900

    공연 관련 데이터 계산 함수들을 담을 클래스 생성

--- src/ch1/after/createStatementData.js
+++ src/ch1/after/createStatementData.js
@@ -1,3 +1,9 @@
+class PerformanceCalculator {
+  constructor(aPerformance) {
+    this.performance = aPerformance;
+  }
+}
+
 export default function createStatementData(invoice, plays) {
   const statementData = {}; // 중간 데이터 구조
   statementData.customer = invoice.customer;
@@ -7,6 +13,7 @@ export default function createStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance);
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = playFor(result);
     result.amount = amountFor(result);
