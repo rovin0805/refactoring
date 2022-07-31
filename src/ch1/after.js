d1eead34773aedb466f0cbd7947ac9aa0a5ca505
Choi Eun Jeong <qhdrn67@naver.com>
2022-07-31 18:15:06 +0900

    적립 포인트 계산 => 중간 데이터

--- src/ch1/after.js
+++ src/ch1/after.js
@@ -11,6 +11,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -39,6 +40,14 @@ function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if (aPerformance.play.type === 'comedy')
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlaintext(data, plays) {
@@ -61,7 +70,7 @@ function renderPlaintext(data, plays) {
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of data.performances) {
-      volumeCredits = volumeCreditsFor(perf);
+      volumeCredits = perf.volumeCredits;
     }
     return volumeCredits;
   }
@@ -72,14 +81,6 @@ function renderPlaintext(data, plays) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if (aPerformance.play.type === 'comedy')
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
 
