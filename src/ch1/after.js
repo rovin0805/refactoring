2437e93f771e57a12e392588ca1c9f5a91afc12e
Choi Eun Jeong <qhdrn67@naver.com>
2022-07-31 17:00:45 +0900

    format 변수 제거

--- src/ch1/after.js
+++ src/ch1/after.js
@@ -5,23 +5,18 @@ function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits = volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
@@ -58,6 +53,14 @@ function volumeCreditsFor(aPerformance) {
   if (playFor(aPerformance).type === 'comedy')
     result += Math.floor(aPerformance.audience / 5);
   return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 console.log(statement(invoices[0], plays));
